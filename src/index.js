c60fbf234e090403560d73de1e6355e6a10aaf57
Eric Taylor <vigne6@gmail.com>
2019-03-12 17:16:07 -0700

    Latitude setup with setstate

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
- render(){
+ constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
+        (position) => {
+            //called setstate
+            this.setState({ lat: position.coords.latitude })
+        }
    );
-    return <div>Latitude:</div>
+ }
+
+
+ render(){
+    
+    return <div>Latitude: {this.state.lat}</div>
  }
 }
 
